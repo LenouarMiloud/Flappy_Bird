0f3121a97e448d7841b6f052c04b342f45ab256c
LenouarMiloud <lenouarmiloud@gmail.com>
2021-02-03 21:09:28 +0100

    modify the AppConstants java class

--- app/src/main/java/robot/fsociety/flappybirdkotlin/AppConstants.java
+++ app/src/main/java/robot/fsociety/flappybirdkotlin/AppConstants.java
@@ -1,4 +1,21 @@
 package robot.fsociety.flappybirdkotlin;
 
+import android.content.Context;
+
 public class AppConstants {
+
+    static bitMapStore BitMapStore;
+    static GameEngine gameEngine;
+
+    public static void initialization(Context context){
+        BitMapStore = new bitMapStore(context.getResources());
+        gameEngine = new GameEngine();
+    }
+
+    //define GetMap Store
+    public static bitMapStore getBitMapStore(){
+        return BitMapStore;
+    }
+
+
 }
