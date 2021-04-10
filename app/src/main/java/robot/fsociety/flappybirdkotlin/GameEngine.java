4d41d0eb6428c3ab059c95b50f6a6d45d2ff53c8
LenouarMiloud <lenouarmiloud@gmail.com>
2021-04-10 12:43:13 +0200

    add the game over test

--- app/src/main/java/robot/fsociety/flappybirdkotlin/GameEngine.java
+++ app/src/main/java/robot/fsociety/flappybirdkotlin/GameEngine.java
@@ -3,6 +3,7 @@ package robot.fsociety.flappybirdkotlin;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,10 +25,10 @@ public class GameEngine {
         gameState = 0;
         tubes = new ArrayList<>();
         random = new Random();
-        for (int i=0;i<AppConstants.numberTubes;i++){
+        for (int i = 0; i < AppConstants.numberTubes; i++) {
             int tubeX = AppConstants.SCREEN_WIDTH + i * AppConstants.distanceTubes;
-            int topTubeOffsetY = AppConstants.minTubeOffSetY + random.nextInt(AppConstants.maxTubeOffSetY - AppConstants.minTubeOffSetY+1);
-            MyTube tube = new MyTube(tubeX,topTubeOffsetY);
+            int topTubeOffsetY = AppConstants.minTubeOffSetY + random.nextInt(AppConstants.maxTubeOffSetY - AppConstants.minTubeOffSetY + 1);
+            MyTube tube = new MyTube(tubeX, topTubeOffsetY);
             tubes.add(tube);
         }
         score = 0;
@@ -38,63 +39,69 @@ public class GameEngine {
         scorePaint.setTextAlign(Paint.Align.LEFT);
     }
 
-    public void updateAndDrawTubes(Canvas canvas){
-        if (gameState == 1){
-            if(tubes.get(scoringTube).getTubeX() < bird.getBirdX() - AppConstants.getBitmapStore().getTubeWidth()){
+    public void updateAndDrawTubes(Canvas canvas) {
+        if (gameState == 1) {
+            if ((tubes.get(scoringTube).getTubeX() < bird.getBirdX() + AppConstants.getBitmapStore().getBirdWidth())
+                    && (tubes.get(scoringTube).getTopTubOffSetY() > bird.getBirdY() || tubes.get(scoringTube).getTubeBottomY() < (bird.getBirdY()
+                    + AppConstants.getBitmapStore().getBirdHeight()))) {
+                //Game Over
+                gameState = 2;
+                Log.d("GAME", "Over");
+            } else if (tubes.get(scoringTube).getTubeX() < bird.getBirdX() - AppConstants.getBitmapStore().getTubeWidth()) {
                 score++;
                 scoringTube++;
-                if(scoringTube > AppConstants.numberTubes - 1 ){
+                if (scoringTube > AppConstants.numberTubes - 1) {
                     scoringTube = 0;
                 }
             }
-            for(int i=0;i<AppConstants.numberTubes;i++){
-                if(tubes.get(i).getTubeX() < -AppConstants.getBitmapStore().getTubeWidth()){
+            for (int i = 0; i < AppConstants.numberTubes; i++) {
+                if (tubes.get(i).getTubeX() < -AppConstants.getBitmapStore().getTubeWidth()) {
                     tubes.get(i).setTubeX(tubes.get(i).getTubeX() + AppConstants.numberTubes * AppConstants.distanceTubes);
-                    int topTubeOffSetY = AppConstants.minTubeOffSetY + random.nextInt(AppConstants.maxTubeOffSetY - AppConstants.minTubeOffSetY + 1 );
+                    int topTubeOffSetY = AppConstants.minTubeOffSetY + random.nextInt(AppConstants.maxTubeOffSetY - AppConstants.minTubeOffSetY + 1);
                     tubes.get(i).setTopTubOffSetY(topTubeOffSetY);
                     tubes.get(i).setTubeColor();
                 }
                 tubes.get(i).setTubeX(tubes.get(i).getTubeX() - AppConstants.tubeVelosity);
-                if(tubes.get(i).getTubeColor() == 0){
-                    canvas.drawBitmap(AppConstants.getBitmapStore().getTubeTop(),tubes.get(i).getTubeX(),tubes.get(i).getTubeTopY(),null);
-                    canvas.drawBitmap(AppConstants.getBitmapStore().getTubeBottom(),tubes.get(i).getTubeX(),tubes.get(i).getTubeBottomY(),null);
-                }else{
-                    canvas.drawBitmap(AppConstants.getBitmapStore().getGreenTubeTop(),tubes.get(i).getTubeX(),tubes.get(i).getTubeTopY(),null);
-                    canvas.drawBitmap(AppConstants.getBitmapStore().getGreenTubeBottom(),tubes.get(i).getTubeX(),tubes.get(i).getTubeBottomY(),null);
+                if (tubes.get(i).getTubeColor() == 0) {
+                    canvas.drawBitmap(AppConstants.getBitmapStore().getTubeTop(), tubes.get(i).getTubeX(), tubes.get(i).getTubeTopY(), null);
+                    canvas.drawBitmap(AppConstants.getBitmapStore().getTubeBottom(), tubes.get(i).getTubeX(), tubes.get(i).getTubeBottomY(), null);
+                } else {
+                    canvas.drawBitmap(AppConstants.getBitmapStore().getGreenTubeTop(), tubes.get(i).getTubeX(), tubes.get(i).getTubeTopY(), null);
+                    canvas.drawBitmap(AppConstants.getBitmapStore().getGreenTubeBottom(), tubes.get(i).getTubeX(), tubes.get(i).getTubeBottomY(), null);
                 }
             }
-            canvas.drawText("Point: "+score,0,110,scorePaint);
+            canvas.drawText("Point: " + score, 0, 110, scorePaint);
         }
     }
 
-    public void updateAndDrawBackgroundImage(Canvas canvas){
+    public void updateAndDrawBackgroundImage(Canvas canvas) {
         backgrounImage.setBackgroundImageX(backgrounImage.getBackgroundImageX() - backgrounImage.getBackgroundImageVelocity());
-        if(backgrounImage.getBackgroundImageX() < -AppConstants.getBitmapStore().getBackgroundWidth()){
+        if (backgrounImage.getBackgroundImageX() < -AppConstants.getBitmapStore().getBackgroundWidth()) {
             backgrounImage.setBackgroundImageX(0);
         }
 
-        canvas.drawBitmap(AppConstants.getBitmapStore().getBackground(),backgrounImage.getBackgroundImageX(),backgrounImage.getBackgroundImageY(),null);
-        if (backgrounImage.getBackgroundImageX() < -(AppConstants.getBitmapStore().getBackgroundWidth() - AppConstants.SCREEN_WIDTH)){
-            canvas.drawBitmap(AppConstants.getBitmapStore().getBackground(),backgrounImage.getBackgroundImageX() + AppConstants.getBitmapStore().getBackgroundWidth(),backgrounImage.getBackgroundImageY(),null);
+        canvas.drawBitmap(AppConstants.getBitmapStore().getBackground(), backgrounImage.getBackgroundImageX(), backgrounImage.getBackgroundImageY(), null);
+        if (backgrounImage.getBackgroundImageX() < -(AppConstants.getBitmapStore().getBackgroundWidth() - AppConstants.SCREEN_WIDTH)) {
+            canvas.drawBitmap(AppConstants.getBitmapStore().getBackground(), backgrounImage.getBackgroundImageX() + AppConstants.getBitmapStore().getBackgroundWidth(), backgrounImage.getBackgroundImageY(), null);
 
         }
 
     }
 
-    public void updateAndDrawBird(Canvas canvas){
+    public void updateAndDrawBird(Canvas canvas) {
 
-        if(gameState == 1){
-            if(bird.getBirdY() < AppConstants.SCREEN_HEIGHT - AppConstants.getBitmapStore().getBirdHeight() || bird.getVelocity() < 0){
+        if (gameState == 1) {
+            if (bird.getBirdY() < AppConstants.SCREEN_HEIGHT - AppConstants.getBitmapStore().getBirdHeight() || bird.getVelocity() < 0) {
                 bird.setVelocity(bird.getVelocity() + AppConstants.gravity);
                 bird.setBirdY(bird.getBirdY() + bird.getVelocity());
             }
         }
 
         int currentFrame = bird.getCurrentFrame();
-        canvas.drawBitmap(AppConstants.getBitmapStore().getBird(currentFrame),bird.getBirdX(),bird.getBirdY(),null);
+        canvas.drawBitmap(AppConstants.getBitmapStore().getBird(currentFrame), bird.getBirdX(), bird.getBirdY(), null);
         currentFrame++;
-        if(currentFrame > Bird.maxFrame){
-            currentFrame =0;
+        if (currentFrame > Bird.maxFrame) {
+            currentFrame = 0;
         }
         bird.setCurrentFrame(currentFrame);
     }
