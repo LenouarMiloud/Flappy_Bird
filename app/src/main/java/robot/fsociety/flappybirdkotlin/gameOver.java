fa8f2ebf91efbaa1594e65efbf9fe7bb77af6cc5
LenouarMiloud <lenouarmiloud@gmail.com>
2021-04-29 00:57:01 +0200

    design the gameOver Layout

---
+++ app/src/main/java/robot/fsociety/flappybirdkotlin/gameOver.java
@@ -0,0 +1,15 @@
+package robot.fsociety.flappybirdkotlin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class gameOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_over);
+    }
+}
